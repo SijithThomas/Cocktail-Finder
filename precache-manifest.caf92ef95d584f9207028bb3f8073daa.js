f48219ec0a221c05f9be0881c88d5399130e9553
SijithThomas <sijiththomas@gmail.com>
2021-10-12 11:58:49 +0530

    Updates

---
+++ precache-manifest.caf92ef95d584f9207028bb3f8073daa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbee955a486a5e36018de6fa2eadc32",
+    "url": "/Cocktail-Finder/index.html"
+  },
+  {
+    "revision": "8a8571d60ac7e89c830b",
+    "url": "/Cocktail-Finder/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/Cocktail-Finder/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Cocktail-Finder/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a8571d60ac7e89c830b",
+    "url": "/Cocktail-Finder/static/js/main.80f49475.chunk.js"
+  },
+  {
+    "revision": "d469d10272deba6b1b23",
+    "url": "/Cocktail-Finder/static/js/runtime-main.6a913a4b.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/Cocktail-Finder/static/media/logo.9a3d2645.svg"
+  }
+]);
